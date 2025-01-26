574824ca0585a170d4463f52ce55e9b610612583
Kosam Omollo <kosamdjango@gmail.com>
2025-01-26 22:15:07 +0300

    [Update] decouple settings module into local base & prod

---
+++ src/medium/settings/local.py
@@ -0,0 +1,16 @@
+from .base import * #noqa
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DAJNGO_SECRET_KEY", default="django-insecure-&d58bbs!&m^d5u7wb-@q+%-l3flmy1teogvgmxsx1lmgcr#t!w")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRSUTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email_backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@kosamtech.dev"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Medium Clone"
