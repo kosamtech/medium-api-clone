6dadcd60444affda67151c9155c18b9e698a7882
Kosam Omollo <kosamdjango@gmail.com>
2025-01-28 17:08:43 +0300

    [typo] Medimun -> Medium

--- src/medium/urls.py
+++ src/medium/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
-admin.site.site_header = "Medimun API Admin"
+admin.site.site_header = "Medium API Admin"
 admin.site.site_title = "Medium API Admin Portal"
 admin.site.index_title = "Welcome to Medium API Portal"
