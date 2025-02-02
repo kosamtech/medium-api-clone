ae5f046ed58272606b5ed112781bdb5432594138
Kosam Omollo <kosamdjango@gmail.com>
2025-02-02 19:25:39 +0300

    [Update] Implemented base common view set to inherit from

--- src/common/views.py
+++ src/common/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .pagination import MediumPagination
+from .renderers import MediumJSONRenderer
+
+
+class GenericModelViewSet(ModelViewSet):
+    renderer_classes = (MediumJSONRenderer,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    permission_classes = [IsAuthenticated]
+    action_serializers = {}
+    pagination_class = MediumPagination
+
+    def get_serializer_class(self):
+        """
+        Return the class to use for the serializer.
+        Defaults to using `self.serializer_class` for
+        create, update & partial_update actions
+        """
+        serializer = self.action_serializers.get(self.action)
+        if serializer:
+            return serializer
+        return super().get_serializer_class()
